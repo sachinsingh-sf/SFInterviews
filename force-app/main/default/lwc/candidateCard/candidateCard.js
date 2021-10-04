0d2b832a03fc8271fb1a258150404243900a4db0
kumar pahlajani <kumar.pahlajani@gmail.com>
2021-10-04 12:18:36 +0530

    component changes

--- force-app/main/default/lwc/candidateCard/candidateCard.js
+++ force-app/main/default/lwc/candidateCard/candidateCard.js
@@ -2,8 +2,15 @@ import { LightningElement, track} from 'lwc';
 
 export default class CandidateCard extends LightningElement {
     @track greeting = " World ";
+    @track mouseEntered = false;
+
     changeHandler(event){
         this.greeting = event.target.value;
+    }
+
+
+    handleSelect(event){
+        mouseEntered = true;
     }
 
 }
